923059a34bbd06328cb19ffe426342f1f3fbf3be
Jarrett Keifer <jkeifer0@gmail.com>
2025-07-22 12:57:52 -0700

    image updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,12 +2,10 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'CCRP',
   tagline: 'Coalesced Chunk Read Protocol',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -20,17 +18,12 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ccrp/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'jkeifer', // Usually your GitHub org/user name.
-  projectName: 'ccrp', // Usually your repo name.
+  organizationName: 'jkeifer',
+  projectName: 'ccrp',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -42,8 +35,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
         },
         blog: false,
         theme: {
@@ -68,13 +59,12 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ccrp-social-card.jpg',
     navbar: {
-      title: 'CCRP',
+      title: 'CCRP: Coalesced Chunk Retrieval Protocol',
       logo: {
         alt: 'CCRP Logo',
-        src: 'img/logo.svg',
+        src: 'img/ccrp.svg',
       },
       items: [
         {
