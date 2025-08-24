ef0ed4a7128464112e4cf35c2c8fc8afd2d07121
Jarrett Keifer <jkeifer0@gmail.com>
2025-08-23 18:11:17 -0700

    move site to ccrp.dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,10 +13,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ccrp-dev.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ccrp/',
+  url: 'https://ccrp.dev',
+  baseUrl: '',
 
   organizationName: 'ccrp-dev',
   projectName: 'ccrp',
